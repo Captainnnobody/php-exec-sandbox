742b7a414f2b76fd5c1e4f408e4b18c6cf9bc878
Captainnnobody <155221397+Captainnnobody@users.noreply.github.com>
2023-12-30 15:05:55 +0530

    Update install_php_exec_sandbox.sh

--- install_php_exec_sandbox.sh
+++ install_php_exec_sandbox.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-
 # Install Apache2
 sudo apt-get update
 sudo apt-get install -y apache2
-
 # Copy the PHP file to the web server directory
 sudo cp phpexecsandbox.php /var/www/html/index.php
-
 # Delete the default index.html file
 sudo rm /var/www/html/index.html
-
 # Restart Apache to apply changes
 sudo systemctl restart apache2
-
 echo "Installation complete. Access the PHP Exec Sandbox at http://your-server-ip/ or http://localhost/"
-
